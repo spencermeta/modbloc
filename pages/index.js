b8bf3b12356f8e225961cae8d59433bc86c58717
unknown <josuelpzti@gmail.com>
2023-11-08 08:16:50 -0300

    add index api

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import { Footer, Navbar } from "../components";
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from "../sections";
+import seguridad from "../public/seguridad.png";
+import tiempo from "../public/tiempo.png";
+import { startingFeaturess } from "../constants";
+import { startingFeatures } from "../constants";
+
+const Page = () => (
+  <div className="bg-primary-black overflow-hidden">
+    <Navbar />
+    <Hero />
+    <div className="relative">
+      <About />
+    </div>
+    <div className="relative">
+      <GetStarted img={seguridad} alt="seguridad" title={<>Seguridad y Privacidad</>} sub="Resguardo" text={startingFeaturess} />
+      <div className="gradient-03 z-0" />
+      <WhatsNew />
+      <div className="gradient-03 z-0" />
+      <GetStarted img={tiempo} alt="tiempo" title={<>Control del Tiempo y Pago</>} sub="Autonomia" text={startingFeatures} />
+    </div>
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+    <Footer />
+  </div>
+);
+
+export default Page;
