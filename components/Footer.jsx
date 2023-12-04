a2e7a69ea60763e50b2e909d77c2e6fbf6017515
xinminsu <sinosu@163.com>
2023-12-04 16:56:23 +0800

    add email

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => (
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">Do you want to collaborate with our project?</h4>
         <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
           {/* <img src="/bitco.png" alt="headset" className="w-[24px] h-[24px] object-contain" /> */}
-          <span className="font-normal text-[16px] text-white">Your help is Important</span>
+          <span className="font-normal text-[16px] text-white">Your help is Important, xinmin.su@hotmail.com </span>
         </button>
       </div>
 
