7676c95898add1cf3e6bdf6d3ac59986a7bc0521
xinminsu <sinosu@163.com>
2023-12-06 16:12:07 +0800

    rename

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 
-## Ziyou
+## Modbloc
  
--- sections/About.jsx
+++ sections/About.jsx
@@ -17,7 +17,7 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title="| What is Ziyou ?" textStyles="text-center" />
+      <TypingText title="| What is Modbloc ?" textStyles="text-center" />
       <motion.p variants={fadeIn("up", "tween", 0.2, 1)} className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white">
         <span className="font-extrabold text-white">Modbloc</span> is a platform based on{" "}
         <span className="font-extrabold text-white">blockchain technology</span> that allows professionals from different industries, such as lawyers,
